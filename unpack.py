4ddeb8b93be82e846d598a4f95cf2e584a7b280a
mendelthedeveloper <mendelthehumble@gmail.com>
2021-11-15 22:37:33 -0800

    Migrated to sqlite
    Able to handle all the data at the same time, needed for handling duplicates

--- unpack.py
+++ unpack.py
@@ -12,6 +12,9 @@ import sqlite3
 
 start = datetime.now()
 
+database = sqlite3.connect('files/sql.db')
+cursor = database.cursor()
+
 def shorten_zip(zipcode):
     #check for candian
     if re.search('[A-Za-z]', zipcode):
@@ -19,9 +22,15 @@ def shorten_zip(zipcode):
     else:
         return zipcode[:5]
 
+def create_table():
+    cursor.execute("DROP TABLE IF EXISTS real_estate_data")
+    cursor.execute("CREATE TABLE real_estate_data(state text, city text, zip text, address text, property_type text, beds text, baths text, sqft text, price int, posted_at text)")
+
+columns_list = ['state', 'city', 'zip', 'address', 'property_type', 'beds', 'baths', 'sqft', 'price', 'posted_at']
+
 data_iterator = pd.read_csv("files/renthub_data.csv",
     iterator = True, chunksize = 10**6,
-    usecols = ['state', 'city', 'zip', 'address', 'property_type', 'beds', 'baths', 'sqft', 'price', 'posted_at'],
+    usecols = columns_list,
     parse_dates = ['posted_at'],
     dtype = {
         'state': str,
@@ -36,9 +45,17 @@ data_iterator = pd.read_csv("files/renthub_data.csv",
     },
 )
 
+create_table()
+
 for current_frame in data_iterator:
     current_frame.dropna(subset=['state', 'city', 'zip', 'address'], inplace=True)
-    current_frame['price'] = current_frame['price'].replace(r'[\$\,]', '').astype(int)
-    current_frame['zip'].apply(shorten_zip)
+    current_frame['price'] = current_frame['price'].replace(r'[\$\,]', '').astype(int).round()
+    current_frame['zip'] = current_frame['zip'].apply(shorten_zip)
+    for row in current_frame.itertuples():
+        row_dict = row._asdict()
+        sql_row = [str(row_dict[field]) for field in columns_list]
+        cursor.execute("INSERT INTO real_estate_data VALUES(?,?,?,?,?,?,?,?,?,?);",sql_row)
+
+database.commit()
 
 print("Program run took: " + str(datetime.now() - start))
